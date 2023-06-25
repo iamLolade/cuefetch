d59852c9dfcd4912a28fac55121903a8a8c15201
Ololade Otemade <adeniyiotemade@gmail.com>
2023-06-25 03:08:39 +0100

    Auth service integrated

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,7 @@ function RootLayout({ children }) {
   return (
     <html lang='en'>
         <body>
+          <Provider>
             <div className='main'>
                 <div className='gradient' />
             </div>
@@ -21,6 +22,7 @@ function RootLayout({ children }) {
               <Nav />
                 {children}
             </main>
+          </Provider>
         </body>
     </html>
   )
